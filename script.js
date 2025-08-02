05e3e65f274b4b6bdee88f5743f1bca373d90674
Tekalign-T7 <tekaligntekola@gmail.com>
2025-08-02 17:02:18 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const password=document.querySelector("#password").textContent.toString();
+const confirm_password=document.querySelector("#confirm_password").textContent.toString(); 
+const button=document.querySelector("button");
+button.addEventListener("click",()=>{
+    const error=document.querySelector("#error-space");
+    
+    if (password==confirm_password){
+        error.append(" ");
+    }else {
+        error.append("The password is not identical");
+    }
+});
